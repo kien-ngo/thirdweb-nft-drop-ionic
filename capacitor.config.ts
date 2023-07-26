87188fecd1ef71abf3adc1411a2d5407d0cf1ad3
Kien Ngo <kenseverus@gmail.com>
2023-07-26 09:20:10 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'thirdweb-nft-drop-ionic',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
