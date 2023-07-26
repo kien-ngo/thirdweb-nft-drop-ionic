4fd6e4228a82b72692696c8cfaca15420d1687fe
Kien Ngo <kenseverus@gmail.com>
2023-07-26 10:00:48 -0400

    Init template

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebProvider>
+      <App />
+    </ThirdwebProvider>
   </React.StrictMode>
 );
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { ConnectWallet } from '@thirdweb-dev/react';
 
 const Tab1: React.FC = () => {
   return (
@@ -16,6 +17,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <ConnectWallet />
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
